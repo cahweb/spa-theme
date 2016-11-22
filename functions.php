b4957759b0206593aa90a8e1e4f25412b5c974fa
Austin Tindle <au389404@ucf.edu>
2016-11-22 11:26:24 -0500

    moved static assets to static/

--- functions.php
+++ functions.php
@@ -105,9 +105,9 @@ function spa_scripts() {
 	wp_enqueue_style( 'spa-style', get_stylesheet_uri() );
 
 
-	wp_enqueue_script( 'spa-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'spa-navigation', get_template_directory_uri() . 'static/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'spa-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'spa-skip-link-focus-fix', get_template_directory_uri() . 'static/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
