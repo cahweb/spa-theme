02371a7ccac50e2f914162e7533dc502487f15c5
Austin Tindle <au389404@ucf.edu>
2016-11-22 13:08:49 -0500

    removed powered by WordPress footer

--- footer.php
+++ footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'spa' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'spa' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'spa' ), 'spa', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
