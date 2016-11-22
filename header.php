d9a5a0e05aba9f01fedf0a194a7c7502179f8dd7
Austin Tindle <au389404@ucf.edu>
2016-11-22 13:45:46 -0500

    basic navigation menu styling

--- header.php
+++ header.php
@@ -30,6 +30,9 @@
 	<header id="masthead" class="site-header" role="banner">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'spa' ); ?></button>
+			<div class="menu-logo">
+				<img src="<?php echo get_template_directory_uri() . '/static/img/navbar/logo.png' ?>">
+			</div>
 			<?php 
 				wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) );
 				wp_nav_menu( array( 'theme_location' => 'secondary', 'menu_id' => 'secondary-menu' ) );
